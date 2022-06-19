65fb041d9dbcfbcacab8169ba2f4c4a8c73aadfe
vuminhu <107785431+vuminhu@users.noreply.github.com>
2022-06-19 15:35:56 +0700

    iuuuuuu

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'heluuuu cj iu',
+    introDesc: `Anh có điều này muốn hỏi cj, Cj nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OKiiiii',
+    title: 'Cj có iu A ko hửmmmm ? 😙',
+    desc: 'cj không trả lời hoặc thoát ra, tức là cực kì thích đó nha :> ',
+    btnYes: 'Yêu ơi là yêu <3',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Cj thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì cj cực cực kì iu A đó 😊😊',
+    mess: 'A biết mà, A cũng thích cj lắm 🥰.',
+    messDesc: 'v đừng bao giờ đánh A nữa nha',
+    btnAccept: 'Ok lunnnnnnn 💖',
+    messLink: 'https://www.facebook.com/vuminhluu123'
+}
